6fc5869581fd74bb587ff31eb7175d75c9085805
Iivan1 <130477700+Iivan1@users.noreply.github.com>
2024-02-01 17:35:17 +0000

    Deploying to gh-pages from @ Iivan1/goit-js-hw-09@bd58ff618b0c8731bbbe740527b498120cc24a6a 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";const e=document.querySelector(".feedback-form");e.querySelector("textarea");e.addEventListener("input",o=>{const t=e.elements.name.value,s=e.elements.message.value;console.log(t,s)});
+//# sourceMappingURL=commonHelpers2.js.map
